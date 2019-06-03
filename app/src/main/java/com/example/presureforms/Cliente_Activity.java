ef07fc5e638b68ad9279aef1fa725e6dc46040e5
danieldcc74 <51111495+danieldcc74@users.noreply.github.com>
2019-06-03 22:05:11 +0200

    actualizado 03/06/2019 clases creadas clientes, empresas, presupuestos

--- app/src/main/java/com/example/presureforms/Cliente_Activity.java
+++ app/src/main/java/com/example/presureforms/Cliente_Activity.java
@@ -5,22 +5,28 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class Cliente_Activity extends AppCompatActivity {
-Button btnIrEmpresa;
+Button btnIrClienteAPresupuesto;
 
-
+    EditText idCliente, nameCliente,lastnameCliente,domCliente,loCliente,cpCliente;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cliente);
-        Button btnIrEmpresa=findViewById(R.id.btnSiguiente);
+        Button btnIrClienteAPresupuesto=findViewById(R.id.btnSiguienteAlPresupuesto);
+        idCliente=(EditText)findViewById(R.id.identificacionCliente);
+        nameCliente=(EditText)findViewById(R.id.nombreCliente);
+        lastnameCliente=(EditText)findViewById(R.id.apellidosCliente);
+        domCliente=(EditText)findViewById(R.id.domicilioCliente);
+        loCliente=(EditText)findViewById(R.id.localidadCliente);
+        cpCliente=(EditText)findViewById(R.id.codigoPostalCliente);
 
-
-        btnIrEmpresa.setOnClickListener(new View.OnClickListener() {
+        btnIrClienteAPresupuesto.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent siguiente =new Intent(Cliente_Activity.this, Empresa_Activity.class);
+                Intent siguiente =new Intent(Cliente_Activity.this, Presupuesto_Activity.class);
                 startActivity(siguiente);
             }
         });
--- app/src/main/java/com/example/presureforms/Empresa_Activity.java
+++ app/src/main/java/com/example/presureforms/Empresa_Activity.java
@@ -1,13 +1,35 @@
 package com.example.presureforms;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class Empresa_Activity extends AppCompatActivity {
-
+Button btnEmpresaAlCliente;
+EditText idEmpresa, nameEmpresa,lastnameEmpresa,domEmpresa,loEmpresa,cpEmpresa;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_empresa_);
+        Button btnEmpresaAlCliente=findViewById(R.id.btnSiguienteAlCliente);
+        idEmpresa=(EditText)findViewById(R.id.identificacionEmpresa);
+        nameEmpresa=(EditText)findViewById(R.id.nombreEmpresa);
+        lastnameEmpresa=(EditText)findViewById(R.id.apellidosEmpresa);
+        domEmpresa=(EditText)findViewById(R.id.domicilioEmpresa);
+        loEmpresa=(EditText)findViewById(R.id.localidadEmpresa);
+        cpEmpresa=(EditText)findViewById(R.id.codigoPostalEmpresa);
+
+
+        btnEmpresaAlCliente.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent siguiente =new Intent(Empresa_Activity.this, Cliente_Activity.class);
+                startActivity(siguiente);
+            }
+        });
+
     }
 }
