cea8b582a7a173b43568546618c24040e3dea499
danieldcc74 <51111495+danieldcc74@users.noreply.github.com>
2019-06-05 00:32:30 +0200

    actualizado 05/06/2019
    borrado de sql y creado tablas para ver pdf

--- app/src/main/java/com/example/presureforms/MenuPresupuestos.java
+++ app/src/main/java/com/example/presureforms/MenuPresupuestos.java
@@ -1,54 +1,61 @@
 package com.example.presureforms;
 
-import android.database.sqlite.SQLiteOpenHelper;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import com.itextpdf.text.DocumentException;
+
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
 
 public class MenuPresupuestos extends AppCompatActivity {
 
-    Button btnActualizar,btnBorrar,btnMostrar;
+    Button btnActualizar, btnBorrar, btnMostrar;
     EditText buscarIdentificacion, buscarfecha, buscarNumero;
+    private PrespuestoEnPdf prespuestoEnPdf;
+    private String[] caberezaEmpresa = {"Hola3", "hola2", "Hola1"};
+    private String shortTExt = "Hola";
+    private String longText = "HOlA HOlA HOlA HOlA HOlA HOlA HOlA HOlA HOlA HOlA HOlA ";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_presupuestos);
 
-    BBDDSQL conexion = new BBDDSQL(this,"bd_clientes",null,1);
+
+        try {
+            prespuestoEnPdf = new PrespuestoEnPdf(getApplicationContext());
+            prespuestoEnPdf.abrirDocumento();
+            prespuestoEnPdf.crearMetadatos("Clientes", "ventas", "autor");
+            prespuestoEnPdf.agregarTitulos("PRESPUESTOS", "Cliente", "daniel");
+            prespuestoEnPdf.crearTabla(caberezaEmpresa, getEmpresa());
+            prespuestoEnPdf.agregarParrafo(shortTExt);
+            prespuestoEnPdf.agregarParrafo(longText);
+            prespuestoEnPdf.cerrarDocumento();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (DocumentException e) {
+            e.printStackTrace();
+        }
 
 
+    }
 
+    public void visorPDF(View view) {
+        prespuestoEnPdf.verPDF();
 
-        btnActualizar=(Button)findViewById(R.id.btnActualizarPrespuesto);
-        btnBorrar=(Button)findViewById(R.id.btnBorrarPresupuesto);
-        btnMostrar=(Button)findViewById(R.id.btnMostrarPrespuesto);
+    }
 
-        buscarIdentificacion=(EditText)findViewById(R.id.buscarXidentificacion);
-        buscarfecha=(EditText)findViewById(R.id.buscarXfecha);
-        buscarNumero=(EditText)findViewById(R.id.buscarXnumero);
+    private ArrayList<String[]> getEmpresa() {
+        ArrayList<String[]> filas = new ArrayList<>();
 
-
-        btnActualizar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-            }
-        });
-        btnBorrar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-            }
-        });
-        btnMostrar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-            }
-        });
-
+        filas.add(new String[]{"1", "apellidos", "segundo"});
+        filas.add(new String[]{"1", "apellidos", "segundo"});
+        filas.add(new String[]{"1", "apellidos", "segundo"});
+        filas.add(new String[]{"1", "apellidos", "segundo"});
+        filas.add(new String[]{"1", "apellidos", "segundo"});
+        return filas;
     }
 }
---
+++ app/src/main/java/com/example/presureforms/PrespuestoEnPdf.java
@@ -0,0 +1,145 @@
+package com.example.presureforms;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Environment;
+import android.util.Log;
+import com.itextpdf.text.*;
+import com.itextpdf.text.pdf.PdfPCell;
+import com.itextpdf.text.pdf.PdfPTable;
+import com.itextpdf.text.pdf.PdfWriter;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.util.ArrayList;
+
+public class PrespuestoEnPdf {
+    private Context context;
+    private File ficheroPdf;
+    private Document document;
+    private PdfWriter escribirPdf;
+    private Paragraph parrafo;
+
+    private Font tituloFuente = new Font(Font.FontFamily.COURIER, 15);
+    private Font subtitulo = new Font(Font.FontFamily.COURIER, 13);
+
+
+    public PrespuestoEnPdf(Context cotext) throws FileNotFoundException, DocumentException {
+        this.context = context;
+        escribirPdf = PdfWriter.getInstance(document, new FileOutputStream(ficheroPdf));
+        document.open();
+    }
+
+    public void abrirDocumento() {
+        creadorCarpeta();
+        try {
+            document = new Document(PageSize.A4);
+        } catch (Exception e) {
+            Log.e("abrirDocumento", e.toString());
+
+        }
+
+    }
+
+    private void creadorCarpeta() {
+
+        File carpeta = new File(Environment.getExternalStorageDirectory().toString(), "PresuReformsPDF");
+
+        if (!carpeta.exists()) {
+            carpeta.mkdirs();
+            ficheroPdf = new File(carpeta, "Factura.pdf");
+
+
+        }
+
+    }
+
+    public void cerrarDocumento() {
+        document.close();
+
+    }
+
+    public void crearMetadatos(String titulo, String sujeto, String autor) {
+        document.addTitle(titulo);
+        document.addSubject(sujeto);
+        document.addAuthor(autor);
+    }
+
+    public void agregarTitulos(String titulo, String sujeto, String autor) {
+        try {
+            parrafo = new Paragraph();
+
+        } catch (Exception e) {
+
+            Log.e("agregarTitulos", e.toString());
+
+        }
+
+
+    }
+
+    private void crearSubtitulo(Paragraph parrafoHijo) {
+        try {
+
+            parrafoHijo.setAlignment(Element.ALIGN_CENTER);
+            parrafo.add(parrafoHijo);
+        } catch (Exception e) {
+
+            Log.e("crearSubtitlo", e.toString());
+
+        }
+    }
+
+    public void crearTabla(String[] cabeberaEmpresa, ArrayList<String[]> empresa) {
+        try {
+            parrafo = new Paragraph();
+            parrafo.setFont(tituloFuente);
+            PdfPTable tablaEmpresa = new PdfPTable(cabeberaEmpresa.length);
+            tablaEmpresa.setWidthPercentage(100);
+            PdfPCell celdas;
+            int caberezaEmpresaPrincipal = 0;
+            while (caberezaEmpresaPrincipal < cabeberaEmpresa.length) {
+                celdas = new PdfPCell(new Phrase(cabeberaEmpresa[caberezaEmpresaPrincipal++], tituloFuente));
+                celdas.setHorizontalAlignment(Element.ALIGN_LEFT);
+                celdas.setBackgroundColor(BaseColor.BLUE);
+                tablaEmpresa.addCell(celdas);
+            }
+
+            for (int indexR = 0; indexR < empresa.size(); indexR++) {
+                String[] filas = empresa.get(indexR);
+                for (int indexC = 0; indexC < cabeberaEmpresa.length; indexC++) {
+                    celdas = new PdfPCell(new Phrase(filas[indexC]));
+                    celdas.setHorizontalAlignment(Element.ALIGN_LEFT);
+                    tablaEmpresa.addCell(celdas);
+                }
+            }
+
+            parrafo.add(tablaEmpresa);
+            document.add(parrafo);
+        } catch (Exception e) {
+            Log.e("crearTabla", e.toString());
+
+        }
+    }
+
+    public void agregarParrafo(String texto) {
+        try {
+            parrafo = new Paragraph(texto, tituloFuente);
+            document.add(parrafo);
+
+        } catch (Exception e) {
+
+            Log.e("agregarTitulos", e.toString());
+
+        }
+    }
+
+    public void verPDF() {
+        Intent siguiente = new Intent(context, VerFicheroPDF.class);
+        siguiente.putExtra("path", ficheroPdf.getAbsolutePath());
+        siguiente.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        context.startActivity(siguiente);
+
+    }
+}
