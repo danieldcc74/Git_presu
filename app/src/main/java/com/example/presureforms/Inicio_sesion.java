6727c2c56f03ea96f23b103bd7c9aa33b6b89944
ddcarmo <danieldcc74@gmail.com>
2019-11-07 16:01:52 +0100

    7/11/2019

---
+++ app/src/main/java/com/example/presureforms/Inicio_sesion.java
@@ -0,0 +1,19 @@
+package com.example.presureforms;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class Inicio_sesion extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_inicio_sesion, container, false);
+    }
+}
