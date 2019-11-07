4275c759beac170b900cc9c05594c548d78c75e6
danieldcc74 <51111495+danieldcc74@users.noreply.github.com>
2019-11-07 22:11:10 +0100

    07/11/2019

--- app/src/main/java/com/example/presureforms/Contacto_Activity.java
+++ app/src/main/java/com/example/presureforms/Contacto_Activity.java
@@ -1,13 +1,27 @@
 package com.example.presureforms;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Contacto_Activity extends AppCompatActivity {
+Button volver_contac_sala;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contacto);
+
+        volver_contac_sala=(Button)findViewById(R.id.btn_volver_contac_sala);
+        volver_contac_sala.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent irSala= new Intent(Contacto_Activity.this,Sala_Principal.class);
+                startActivity(irSala);
+            }
+        });
     }
+
 }
---
+++ app/src/main/java/com/example/presureforms/Login_activity.java
@@ -0,0 +1,60 @@
+package com.example.presureforms;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.JsonRequest;
+import com.android.volley.toolbox.Volley;
+import org.json.JSONObject;
+
+public class Login_activity extends AppCompatActivity implements Response.Listener<JSONObject>, Response.ErrorListener {
+    RequestQueue solicitud;
+    JsonRequest jSolicitud;
+
+    EditText box_correo, box_contraseña;
+    Button btn_iniciar_sesion;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        solicitud = Volley.newRequestQueue(this);
+
+
+        box_correo = (EditText) findViewById(R.id.box_email);
+        box_contraseña = (EditText) findViewById(R.id.box_password);
+        btn_iniciar_sesion = (Button) findViewById(R.id.btn_iniciar_sesion);
+
+        btn_iniciar_sesion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                metodo_iniciar_sesion();
+            }
+        });
+    }
+
+    private void metodo_iniciar_sesion() {
+        String url = "poner la ip de mi hosting/user=" + box_correo.getText().toString() + "&pwd=" + box_contraseña.getText().toString();
+
+        jSolicitud = new JsonObjectRequest(Request.Method.GET,url,null,this,this);
+        solicitud.add(jSolicitud);
+    }
+
+    @Override
+    public void onErrorResponse(VolleyError error) {
+        Toast.makeText(this, "Algo has puesto mal", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onResponse(JSONObject response) {
+        Toast.makeText(this, "Bingo!!", Toast.LENGTH_SHORT).show();
+    }
+}
