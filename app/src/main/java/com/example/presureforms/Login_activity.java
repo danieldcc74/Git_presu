8ceaa779a330ac2ae0904001a05217afcc1d515e
danieldcc74 <51111495+danieldcc74@users.noreply.github.com>
2019-11-13 00:13:01 +0100

    13 empezando con login y casi acabado

--- app/src/main/java/com/example/presureforms/Login_activity.java
+++ app/src/main/java/com/example/presureforms/Login_activity.java
@@ -7,21 +7,20 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
-import com.android.volley.Request;
-import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.*;
 import com.android.volley.toolbox.JsonRequest;
+import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
-import org.json.JSONObject;
 
-public class Login_activity extends AppCompatActivity implements Response.Listener<JSONObject>, Response.ErrorListener {
+import java.util.HashMap;
+import java.util.Map;
+
+public class Login_activity extends AppCompatActivity {
     RequestQueue solicitud;
     JsonRequest jSolicitud;
 
     EditText box_correo, box_contraseña;
-    Button btn_iniciar_sesion ,btn_registro;
+    Button btn_iniciar_sesion, btn_registro;
 
 
     @Override
@@ -39,32 +38,47 @@ public class Login_activity extends AppCompatActivity implements Response.Listen
         btn_registro.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent irRegistro =new Intent (Login_activity.this, Registro_activity.class);
+                Intent irRegistro = new Intent(Login_activity.this, Registro_activity.class);
                 startActivity(irRegistro);
             }
         });
         btn_iniciar_sesion.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                metodo_iniciar_sesion();
+                metodo_iniciar_sesion("http://192.168.0.13:80/registro/validar_usuario.php");
             }
         });
     }
 
-    private void metodo_iniciar_sesion() {
-        String url = "https://91.199.120.16/inicio_sesion/inicio_sesion.php/user=" + box_correo.getText().toString() + "&pwd=" + box_contraseña.getText().toString();
+    private void metodo_iniciar_sesion(String URL) {
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, URL, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                    if(response.isEmpty()){
+                        Intent irSalaPrincipal = new Intent(Login_activity.this, Sala_Principal.class);
+                        startActivity(irSalaPrincipal);
+                    }else{
+                        Toast.makeText(Login_activity.this, "El usuario y la contraseña no existen", Toast.LENGTH_SHORT).show();
 
-        jSolicitud = new JsonObjectRequest(Request.Method.GET,url,null,this,this);
-        solicitud.add(jSolicitud);
+                    }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(Login_activity.this, error.toString(), Toast.LENGTH_SHORT).show();
+            }
+        }) {
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String,String> parametros = new HashMap<String,String>();
+                parametros.put("email", box_correo.getText().toString());
+                parametros.put("contraseña", box_contraseña.getText().toString());
+
+                return parametros;
+            }
+        };
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        requestQueue.add(stringRequest);
+
     }
 
-    @Override
-    public void onErrorResponse(VolleyError error) {
-        Toast.makeText(this, "Algo has puesto mal", Toast.LENGTH_SHORT).show();
-    }
-
-    @Override
-    public void onResponse(JSONObject response) {
-        Toast.makeText(this, "Bingo!!", Toast.LENGTH_SHORT).show();
-    }
 }
